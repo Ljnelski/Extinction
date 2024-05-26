b66ac64bf1fba29b6d890ce1acd49a6d2bb8be7b
Ljnelski <Ljnelski@gmail.com>
2024-05-26 15:08:47 -0400

    Fixed Unity Project Existing inside its own folder instead of the root folder

---
+++ Assets/UI/Menu/Menu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void SwitchToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        if (Application.isEditor)
+        {
+            EditorApplication.ExitPlaymode();
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
