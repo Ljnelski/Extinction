b7a5c52ed4a6cbad2fc07cdfbb1e512d570aef1c
CursiveCrow <34394708+CursiveCrow@users.noreply.github.com>
2024-05-27 11:22:01 -0700

    Level Work
    Initial block-in of level.

---
+++ Assets/Content/Asset List.cs
@@ -0,0 +1,17 @@
+// A complete list of all 3rd party assets used in this project:
+
+/*
+- Terrain Stamps: https://github.com/Roland09/Terrain-Stamps
+
+- Mountain Pack: https://assetstore.unity.com/packages/3d/environments/landscapes/mountain-pack-30608
+
+- Autumn Mountain: https://assetstore.unity.com/packages/3d/environments/landscapes/autumn-mountain-52251
+
+- Allsky Free
+
+- Quixel Megascans Rocks + Materials
+
+- Tree Collection Pack: https://assetstore.unity.com/packages/3d/vegetation/trees/tree-collection-pack-2017-76974
+
+
+*/
